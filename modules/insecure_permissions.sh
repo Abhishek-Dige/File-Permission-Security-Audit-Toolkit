f8745d28526f34ab1c2c12b24349bac3a052be45
Atul-Pal <kingironcobra3000@gmail.com>
2025-12-07 22:44:11 +0530

    Clean up blank lines in insecure_permissions.sh
    Removed unnecessary blank lines for cleaner code.

--- modules/insecure_permissions.sh
+++ modules/insecure_permissions.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-
-
 OUTPUT="../output/insecure_permissions.txt"
 FILES="../tests"
-
-
 
 # Start report
 echo "---Insecure Permission Report---" > "$OUTPUT"
 echo "Generated on: $(date)" >> "$OUTPUT"
 echo "" >> "$OUTPUT"
 
-
-
-
-
 # Detect files with 777 permissions
-
 echo "---Files with 777 permissions---:" >> "$OUTPUT"
 find "$FILES" -type f -perm 777 2>/dev/null >> "$OUTPUT"
 echo "" >> "$OUTPUT"
-
-
 
 # Detect directories with 777 permissions
 echo "---Directories with 777 permissions---:" >> "$OUTPUT"
 find "$FILES" -type d -perm 777 2>/dev/null  >> "$OUTPUT"
 echo "" >> "$OUTPUT"
-
-
 
 echo "Scan completed. Report saved to $OUTPUT"
