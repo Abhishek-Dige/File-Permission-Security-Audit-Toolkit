29b5c6625752f2eac2c6836943bed634f90c7ae6
Abhishek-Dige <abhishekdige22@gmail.com>
2025-12-08 04:39:55 +0000

    commmiting small changes

--- modules/weak_script_detector.sh
+++ modules/weak_script_detector.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Asking the user to enter absolute path of the direcory to be scanned
+
 
 scan_directory="../tests"
 output_file="../output/weak_script_report.txt"
 
-# Generating the head of the weak script report file
+
 
 echo "Weak script report" > "$output_file"
 echo "Generated on : $(date)" >> "$output_file"
 echo "Directory scanned : $scan_directory" >> "$output_file"
 echo "------------------------------------------------------" >> "$output_file"
 
-# Checking for missing SHEBANG LINE
+
 
 echo "" >> "$output_file"
 echo "1) Scripts missing SHEBANG LINE" >> "$output_file"
@@ -21,14 +21,13 @@ echo "------------------------------------------------------" >> "$output_file"
 find "$scan_directory" -type f -name "*.sh" | while read file
 do
         first_line=$(head -n 1 "$file")
-        if [[ "$first_line" != "#!/bin/bash" ]]
+        if [[ "$first_line" != "#!"* ]]
         then
                 echo "$file" >> "$output_file"
         fi
 done
 
 
-# Checking for risky patterns
 
 echo "" >> "$output_file"
 echo "2) Scripts containing risky Patterns" >> "$output_file"
